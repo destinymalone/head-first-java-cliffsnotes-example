328d530970b6e83448b27c9e2f09516af0c906d9
Destiny Malone <dmalone@basecampcodingacademy.org>
2020-01-24 14:05:06 -0600

    Providing a visual for overloading methods.

---
+++ chapter-7-inheritance-and-polymorphism-better-living-in-objectville/Overloading.java
@@ -0,0 +1,7 @@
+class Overloading extends DisplayOverloading {
+    public static void main(String[] args) {
+        DisplayOverloading obj = new DisplayOverloading();
+        obj.disp('a');
+        obj.disp('a', 10);
+    }
+}
